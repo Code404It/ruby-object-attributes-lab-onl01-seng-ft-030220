e73d73bed05a33f3721dd47a31e95b9e4ca9c5e6
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-15 02:28:16 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+def name 
+  @name 
+end 
+
+def name=(dog_name)
+  @name=dog_name 
+end 
+
+def breed
+  @breed 
+end
+  
+def breed=(beagle)
+  @breed=beagle 
+   snoopy=Dog.new
+    snoopy.breed="Beagle"
+ end
+end 
